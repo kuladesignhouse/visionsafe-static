c0eca288c7c5eb39d5fe28cc04c401431dc7943c
pydevcs <pydevcs@gmail.com>
2021-12-23 16:06:18 -0800

    Expertrec search demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
- 
+var sitemap = require('gulp-sitemap'); 
+
 exports.default = () => (
     gulp.src('templates/*.html')
         .pipe(nunjucks.compile())
         .pipe(gulp.dest('dist'))
 );
+
+gulp.task('sitemap', function () {
+  return gulp.src('dist/**/*.html', {
+      read: false
+    })
+    .pipe(sitemap({
+      siteUrl: 'https://www.visionsafe.com'
+    }))
+    .pipe(gulp.dest('dist'));
+});
