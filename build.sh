f897f479328ade5891fb53bacfb8d57d0d2ed508
pydevcs <pydevcs@gmail.com>
2025-06-11 10:44:14 -0700

    about us / index changes

--- build.sh
+++ build.sh
@@ -1,1 +1,21 @@
-gulp & scp /Users/casey/Documents/dev/work/visionsafe-static/dist/bombardier.html brgr@visionsafe.com:/home/brgr/visionsafe.com/bombardier.html
+#!/bin/bash
+
+# Run the Gulp task
+gulp
+
+# Check if the Gulp task was successful
+if [ $? -eq 0 ]; then
+    # Transfer the file to the server using scp
+    scp ./dist/index.html brgr@visionsafe.com:/home/brgr/visionsafe.com/index.html
+
+    # Check if the file transfer was successful
+    if [ $? -eq 0 ]; then
+        echo "File transferred successfully."
+        afplay /System/Library/Sounds/Purr.aiff
+    else
+        echo "File transfer failed."
+    fi
+else
+    echo "Gulp task failed."
+fi
+
